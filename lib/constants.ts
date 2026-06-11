d468f32f0daf98e7961855d0ddd24be4d60774e2
wangcongfelix <1836939455@qq.com>
2026-06-11 20:36:48 +0800

    refactor: remove legacy resume extraction code

--- lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const MODEL = 'gpt-4o-mini'
-export const EXTRACT_VALUES_PROMPT =
-  "Extract the relevant fields from the resume text provided. If you can't identify a required field, leave it blank."
